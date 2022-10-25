105b4a1da992488b701fd9d37d6310409f6b11aa
yangm9 <yangm9@icloud.com>
2022-10-25 20:24:36 +0800

    v0.0.1

--- src/assemble/assembly.py
+++ src/assemble/assembly.py
@@ -10,10 +10,13 @@ def spades(fastqs: list, group: str, outdir: str):
     spades_dir = f'{outdir}/01.assembly/{group}'
     if not os.path.exists(spades_dir): os.makedirs(spades_dir)
     spades_cmd.extend(['spades.py', '--pe1-1', fastqs[0], '--pe1-2', fastqs[1], '--careful', '-t 30 -m 1300 -k 21,33,55,77,99,127', '-o', spades_dir])
-    results=cmdExec.execute(spades_cmd)
+    results = cmdExec.execute(spades_cmd)
     return results
 
 def filtFastA(grp: str, outdir: str, cutoff: int):
+    '''
+    Filter the fasta sequence by length (cutoff).
+    '''
     wkdir = f'{outdir}/01.assembly/{grp}'
     scaffolds = f'{wkdir}/scaffolds.fasta'
     filt_fa_prifix = f'{wkdir}/scaffolds.filt'
@@ -37,9 +40,9 @@ def Assemble(config: str, outdir: str):
         fastq_1 = f'{outdir}/01.assembly/0.fastq/{grp}_1.fq'
         fastq_2 = f'{outdir}/01.assembly/0.fastq/{grp}_2.fq'
         fastqs = [fastq_1, fastq_2]
-        results+=spades(fastqs, grp, outdir)
-        results+=filtFastA(grp, outdir, '2000')
-        results+=filtFastA(grp, outdir, '5000')
-        results+=filtFastA(grp, outdir, '10000')
-        results+=statFastA(grp, outdir)
+        results += spades(fastqs, grp, outdir)
+        results += filtFastA(grp, outdir, '2000')
+        results += filtFastA(grp, outdir, '5000')
+        results += filtFastA(grp, outdir, '10000')
+        results += statFastA(grp, outdir)
     return results
--- src/identify/viridsop.py
+++ src/identify/viridsop.py
@@ -2,11 +2,76 @@
 from ..config import setVari,conf
 from ..process import cmdExec
 
+groups, confDict, sampDict = conf.prepInfo(config)
+
 def findVir(grp: str, outdir: str):
     envs = setVari.selectENV('VirCraft')
-    virsorter_cmd = [envs]
+    find_vir_cmd = [envs]
     identify_dir = f'{outdir}/02.identify/{grp}'
-    scaffolds = f'{outdir}/01.assembly/{grp}/scaffolds.fasta'
     if not os.path.exists(identify_dir): os.makedirs(identify_dir)
-    virsorter_cmd.extend(['virsorter', 'run', '--keep-original-seq', '-i', scaffolds, '-d ', '--include-groups dsDNAphage,ssDNA --min-length 0 --min-score 0.5 -j 28 all', '-o', spades_dir])
+    vs2_pass1_dir = f'{identify_dir}/vs2-pass1'
+    if not os.path.exists(vs2_pass1_dir): os.makedirs(vs2_pass1_dir)
+    
+    scaffolds = f'{outdir}/01.assembly/{grp}/scaffolds.filt.gt5000.fasta'
+    find_vir_cmd.extend(
+        ['virsorter', 'run', '--keep-original-seq', '-i', scaffolds, 
+         '-d ', confDict['virsorter2DB'], '-v', vs2_pass1_dir, 
+         '--include-groups dsDNAphage,ssDNA', 
+         '--min-length 0 --min-score 0.5 -j 28 all\n']
+    )
+
+    vs2_pass1_fasta=f'{vs2_pass0}/final-viral-combined.fa'
+    checkv_dir=f'{identify_dir}/checkv'
+    if not os.path.exists(checkv_dir): os.makedirs(checkv_dir)
+    find_vir_cmd.extend(
+        ['checkv', 'end_to_end', vs2_pass1_fasta, checkv_dir,
+         '-d', confDict['CheckVDB'], '-t 32']
+    )
+
+    provir_fna=f'{checkv_dir}/proviruses.fna'
+    vir_fna=f'{checkv_dir}/viruses.fna'
+    combined_fna=f'{checkv_dir}/combined.fna'
+    find_vir_cmd.extend(
+        ['cat', provir_file, vir_file, combined_fna, '\n']
+    )
+
+    vs2_pass2_dir = f'{identify_dir}/vs2-pass2'
+    if not os.path.exists(vs2_pass2_dir): os.makedirs(vs2_pass2_dir)
+    find_vir_cmd.extend(
+        ['virsorter', 'run', '--seqname-suffix-off', '--viral-gene-enrich-off',         '--provirus-off', '--prep-for-dramv', '-i', combined_fna, 
+         '-w', vs2_pass2_dir, '--include-groups dsDNAphage,ssDNA',
+         '--min-length 5000 --min-score 0.5 -j 28 all\n'
+    )
+
+    dramv_annotate_dir = f'{identify_dir}/dramv-annotate'
+    if not os.path.exists(dramv_annotate_dir): os.makedirs(dramv_annotate_dir)
+    vs2_pass2_fasta = f'{vs2_pass2_dir}/for-dramv/final-viral-combined-for-dramv.fa'
+    vs2_pass2_tab = f'{vs2_pass2_dir}/for-dramv/viral-affi-contigs-for-dramv.tab'
+    find_vir_cmd.extend(
+        ['DRAM-v.py annotate', '-i', vs2_pass2_fasta, '-v', vs2_pass2_tab,  
+         '-o', dramv_annotate_dir, '--skip_trnascan --threads 28 --min_contig_size 1000\n'
+    )
+
+    dramv_distill_dir = f'{identify_dir}/dramv-distill'
+    dramv_annot = f'{dramv_annotate_dir}/annotations.tsv'
+    find_vir_cmd.extend(
+        ['DRAM-v.py distill', '-i', dramv_annot, '-o', dramv_distill_dir]  
+    )
+
+    curation_dir = f'{identify_dir}/curation'
+    vir_score = f'{vs2-pass2_dir}/final-viral-score.tsv'
+    curation_score = f'{curation_dir}/final-viral-score.tsv'
+    contamination = f'{checkv_dir}/contamination.tsv'
+    find_vir_cmd.extend(
+        ["sed '1s/seqname/contig_id/'", vir_score, '>', curation_score, '\n']
+    )
+    
+
+
+    
+
+
+
+    
+
 
