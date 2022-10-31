d5a90460b6f2cccc9188264752d880daa23cf222
yangm9 <yangm9@icloud.com>
2022-11-01 00:17:26 +0800

    v0.0.1

--- bin/specTab4barplot.py
+++ bin/specTab4barplot.py
@@ -2,26 +2,32 @@
 import sys
 import pandas as pd
 
-def sumAbundByTaxa(TaxaAbund):
+def sumAbundByTaxa(taxa_tpm: str, outdir: str):
     '''
     Sum the abundance by Taxa for each sample.
     '''
-    df = pd.read_csv(TaxaAbund_f, sep='\t', header=0)
-    df = df.iloc[:, 0:-2]
+    df = pd.read_csv(taxa_tpm, sep='\t', header=0)
+    samp_num = df.columns.size - 4
+    df = df.iloc[:, 0:samp_num]
+    df['Contig'] = df['Contig'].apply(lambda x:x.split(':')[1])
+    df['Contig'].replace('nan;nan','Unassigned',inplace=True)
     TaxaList = df['Contig'].unique().tolist()
     df = df.set_index('Contig')
     sumDF = pd.DataFrame()
     for i in range(len(TaxaList)):
         series = df.loc[TaxaList[i]]
-        if isinstance(series,pd.DataFrame):
-            series = pd.Series(series.sum(),name=TaxaList[i])
+        if isinstance(series, pd.DataFrame):
+            series = pd.Series(series.sum(), name=TaxaList[i])
         sumDF = sumDF.append(series)
-
     sumDF.reset_index(inplace=True)
     sumDF.rename(columns={'index':'Contig'}, inplace=True)
     sumDF.fillna(0, inplace=True)
-    sumDF.to_csv('tax_tpm.xls', sep='\t',index=False)
+    sumed_tpm=f'{outdir}/tax_sumed_tpm.xls'
+    sumDF.to_csv(sumed_tpm, sep='\t', index=False)
     return 0
 
 if __name__=='__main__':
-    sumAbundByTaxa(sys.argv[1],sys.argv[2])
+    if len(sys.argv) == 3:
+        sumAbundByTaxa(sys.argv[1],sys.argv[2])
+    else:
+        print(f'Usage: python {sys.argv[0]} <merged_anno_tpm.xls> <outdir>')
