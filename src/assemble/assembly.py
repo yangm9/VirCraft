2258a4f931e1beeb041536b9e117ee8f87db6aea
yangm9 <yangm9@icloud.com>
2022-11-03 01:09:23 +0800

    v0.0.1

--- src/assemble/assembly.py
+++ src/assemble/assembly.py
@@ -1,58 +1,66 @@
 #!/usr/bin/env python3
 
 import sys,os
-from ..config import setVari,conf
+from ..config import setVari
 from ..process import cmdExec, general
-from ..fastqc import mergeRead
+from ..fastqc.reads import Reads
 
-def spades(fastqs: list, group: str, outdir: str):
+class Assembly(Reads):
     '''
-    Assemble metagenome by SPAdes for single group.
     '''
-    envs = setVari.selectENV('VirCraft')
-    spades_cmd = [envs]
-    spades_dir = f'{outdir}/01.assembly/{group}'
-    general.mkdir(spades_dir)
-    spades_cmd.extend(['spades.py', '--pe1-1', fastqs[0], '--pe1-2', fastqs[1], '--careful', '-t 30 -m 1300 -k 21,33,55,77,99,127', '-o', spades_dir])
-    spades_sh = f'{outdir}/01.assembly/{group}_spades.sh'
-    general.printSH(spades_sh, spades_cmd)
-    results = cmdExec.execute(spades_cmd)
-    return results
-
-def filtFastA(grp: str, outdir: str, cutoff: int):
-    '''
-    Filter the fasta sequence by length (cutoff).
-    '''
-    wkdir = f'{outdir}/01.assembly/{grp}'
-    scaffolds = f'{wkdir}/scaffolds.fasta'
-    filt_fa_prifix = f'{wkdir}/scaffolds.filt'
-    filt_cmd = ['SeqLenCutoff.pl', scaffolds, filt_fa_prifix, cutoff]
-    filt_sh = f'{wkdir}/filt_scaffolds.sh'
-    general.printSH(filt_sh, filt_cmd)
-    results = cmdExec.execute(filt_cmd)
-    return results
-
-def statFastA(grp: str, outdir: str):
-    wkdir = f'{outdir}/01.assembly/{grp}'
-    contigs = f'{wkdir}/scaffolds.fasta'
-    scaffolds = f'{wkdir}/scaffolds.fasta'
-    stat_tab = f'{wkdir}/stat.tab'
-    stat_cmd = ['assemb_stat.pl', contigs, scaffolds, f'>{stat_tab}\n']
-    stat_sh = f'{wkdir}/stat_fasta.sh'
-    general.printSH(stat_sh, stat_cmd)
-    results = cmdExec.execute(stat_cmd)
-    return results
-
-def Assemble(config: str, outdir: str):
-    mergeRead.MergeFastqByGroup(config,outdir)
-    results=''
-    for grp in mergeRead.groups:
-        fastq_1 = f'{outdir}/01.assembly/0.fastq/{grp}_1.fq'
-        fastq_2 = f'{outdir}/01.assembly/0.fastq/{grp}_2.fq'
-        fastqs = [fastq_1, fastq_2]
-        results += spades(fastqs, grp, outdir)
-        results += filtFastA(grp, outdir, '2000')
-        results += filtFastA(grp, outdir, '5000')
-        results += filtFastA(grp, outdir, '10000')
-        results += statFastA(grp, outdir)
-    return results
+    def __init__(self, config, outdir):
+        Reads.__init__(self, config, outdir)
+        self.envs = setVari.selectENV('VirCraft')
+        self.spades_dir = f'{self.wkdir}/1.spades'
+        general.mkdir(self.spades_dir)
+    def spades(self, fastqs: list, group: str):
+        '''
+        Assemble metagenome by SPAdes for single group.
+        '''
+        spades_cmd = [self.envs]
+        wkdir = f'{self.spades_dir}/{group}'
+        general.mkdir(wkdir)
+        spades_cmd.extend(
+            ['spades.py', '--pe1-1', fastqs[0], '--pe1-2', fastqs[1],
+            '--careful', '-t 30 -m 1300 -k 21,33,55,77,99,127', 
+            '-o', wkdir]
+        )
+        spades_sh = f'{self.wkdir}/{group}_spades.sh'
+        general.printSH(spades_sh, spades_cmd)
+        results = cmdExec.execute(spades_cmd)
+        return results
+    def filtFastA(self, grp: str, cutoff: int):
+        '''
+        Filter the fasta sequence by length (cutoff).
+        '''
+        wkdir = f'{self.wkdir}/{grp}'
+        scaffolds = f'{wkdir}/scaffolds.fasta'
+        filt_fa_prifix = f'{wkdir}/scaffolds.filt'
+        filt_cmd = ['SeqLenCutoff.pl', scaffolds, filt_fa_prifix, cutoff]
+        filt_sh = f'{wkdir}/filt_scaffolds.sh'
+        general.printSH(filt_sh, filt_cmd)
+        results = cmdExec.execute(filt_cmd)
+        return results
+    def statFastA(grp: str, outdir: str):
+        wkdir = f'{outdir}/01.assembly/{grp}'
+        contigs = f'{wkdir}/scaffolds.fasta'
+        scaffolds = f'{wkdir}/scaffolds.fasta'
+        stat_tab = f'{wkdir}/stat.tab'
+        stat_cmd = ['assemb_stat.pl', contigs, scaffolds, f'>{stat_tab}\n']
+        stat_sh = f'{wkdir}/stat_fasta.sh'
+        general.printSH(stat_sh, stat_cmd)
+        results = cmdExec.execute(stat_cmd)
+        return results
+    
+    def Assemble(config: str, outdir: str):
+        results=''
+        for grp in self.groups:
+            fastq_1 = f'{self.fq_dir}/{grp}_1.fq'
+            fastq_2 = f'{self.fq_dir}/{grp}_2.fq'
+            fastqs = [fastq_1, fastq_2]
+            results += spades(fastqs, grp, outdir)
+            results += filtFastA(grp, outdir, '2000')
+            results += filtFastA(grp, outdir, '5000')
+            results += filtFastA(grp, outdir, '10000')
+            results += statFastA(grp, outdir)
+        return results
---
+++ src/fastqc/reads.py
@@ -0,0 +1,44 @@
+import os
+from ..config.cfgInfo import VirCfg
+from ..process import cmdExec, general
+
+class Reads(VirCfg):
+    '''
+    FastQ processing class.
+    '''
+    def __init__(self, config, outdir):
+        VirCfg.__init__(self, config)
+        self.outdir = os.path.abspath(outdir)
+        self.wkdir = f'{self.outdir}/01.assembly'
+        general.mkdir(self.wkdir)
+        self.fq_dir = f'{self.wkdir}/0.fastq'
+        general.mkdir(fq_dir)
+        self.grpFq1Dict, self.grpFq2Dict = self.grpReads
+    @property
+    def grpReads(self):
+        groupFq1Dict = {}
+        groupFq2Dict = {}
+        for samp in self.sampDict.keys():
+            group = self.sampDict[samp][1]
+            fastq1, fastq2 = self.sampDict[samp][2].split(',')
+            groupFq1Dict.setdefault(group, [])
+            groupFq2Dict.setdefault(group, [])
+            groupFq1Dict[group].append(fastq1)
+            groupFq2Dict[group].append(fastq2)
+        return groupFq1Dict, groupFq2Dict
+    def catFqCMD(self, fastqs: list, grp: str, n: int):
+        merge_fq_cmd = ['cat']
+        merge_fq_cmd.extend(fastqs)
+        merge_fq_cmd.append(f'>{self.fq_dir}/{grp}_{n}.fq\n')
+        return merge_fq_cmd
+    @property
+    def MergeFastqByGroup(self):
+        results1,results2 = '',''
+        for grp in self.groups:
+            merge_fq1_cmd = self.catFqCMD(self.grpFq1Dict[grp], grp, 1)
+            merge_fq2_cmd = self.catFqCMD(self.grpFq2Dict[grp], grp, 2)
+            merge_fq_sh=f'{self.wkdir}/{grp}_merge_fq.sh'
+            general.printSH(merge_fq_sh,merge_fq1_cmd+merge_fq2_cmd)
+            results1 += cmdExec.execute(merge_fq1_cmd)
+            results2 += cmdExec.execute(merge_fq2_cmd)
+        return results1, results2
