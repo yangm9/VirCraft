1f20806dc5385b25abe316a4968ace84617d4e1a
yangm9 <yangm9@icloud.com>
2022-12-12 16:28:27 +0800

    v0.0.3

--- install.sh
+++ install.sh
@@ -5,24 +5,8 @@ source ~/.bashrc
 pip install -r requirements.txt
 conda create -y -n VirCraft megahit -c bioconda
 conda activate VirCraft
-conda install -y -c bioconda trimmomatic prodigal spades fastp sortmerna bowtie2 virsorter=2
-conda install -y checkv
-conda install -y dram
-conda install -y -c bioconda cd-hit
-conda install -y -c bioconda eggnog-mapper
-conda install -y -c bioconda vibrant==1.2.1
-conda install -y iqtree
-conda install -y -c bioconda diamond
-conda install -y coverm
-conda install -c bioconda crass
-conda install -c bioconda -y minced
-conda install -c bioconda trnascan-se
-conda install -y salmon
-conda install -y r-base
-conda install -c bioconda hmmer
-conda install -c bioconda muscle
-conda install -y drep
-conda install -c bioconda aragorn
+conda install -y -c bioconda trimmomatic prodigal spades fastp sortmerna bowtie2 virsorter=2 checkv dram cd-hit eggnog-mapper vibrant==1.2.1 iqtree diamond coverm crass minced trnascan-se salmon hmmer muscle drep aragorn kofamscan
+conda install -c conda-forge r-base
 ## Install usearch
 cd ~/VirCraft/bin
 wget https://www.drive5.com/downloads/usearch11.0.667_i86linux32.gz
