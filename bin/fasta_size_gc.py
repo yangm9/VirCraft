9279bd3282c9986cc8b056f1cc95fa53f9055b49
yangm9 <yangm9@qq.com>
2022-12-16 11:03:45 +0800

    v0.0.3

---
+++ bin/fasta_size_gc.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import sys
+from Bio import SeqIO
+
+def seqGcLen(seq:str):
+    length=len(seq)
+    gc_perc=round((seq.count('C')+seq.count('G'))/length*100
+    return length,gc_perc
+
+def statFasta(fasta:str):
+    print('SeqID\tGCcontent\tLength\n')
+    for record in SeqIO.parse(fasta,'fasta'):
+        length,gc_perc=getGC(str(record.seq))
+        length=str(length)
+        gc_perc=str(gc_perc)
+        print(f'{record.id}\t{length}\t{gc_perc}')
+    return 0
+
+if __name__=='__main__':statFasta(sys.argv[1])
