47639add1aa72423406e5f5652607b4d810332fc
yangm9 <yangm9@qq.com>
2022-12-16 11:09:46 +0800

    v0.0.3

--- bin/fasta_size_gc.py
+++ bin/fasta_size_gc.py
@@ -4,7 +4,7 @@ from Bio import SeqIO
 
 def seqGcLen(seq:str):
     length=len(seq)
-    gc_perc=round((seq.count('C')+seq.count('G'))/length*100
+    gc_perc=round((seq.count('C')+seq.count('G'))/length*100)
     return length,gc_perc
 
 def statFasta(fasta:str):
