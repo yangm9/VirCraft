0aeca19049012a8cc008c6fe4cdc4e5367d27b8f
yangm9 <yangm9@icloud.com>
2022-12-23 10:29:34 +0800

    v0.0.4

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ virsorter=2 checkv=0.7.0 dram=1.3.5 cd-hit=4.8.1 eggnog-mapper=2.1.7 \
 vibrant=1.2.1 iqtree=2.2.0.3 diamond=2.0.15 coverm=0.6.1 crass=1.0.1 \
 minced=0.4.2 trnascan-se=2.0.11 salmon=0.14.2 hmmer=3.3.2 muscle=5.1 \
 drep=3.3.1 aragorn=1.2.41 kofamscan=1.3.0 fastuniq=1.1
-conda install -c conda-forge r-base=4.1.3 r-ggplot2=
+conda install -c conda-forge r-base=4.2.1 r-ggplot2=3.4.0 r-vctrs=0.5.1
 pip install -r requirements.txt
 ## Install usearch
 cd ~/VirCraft/bin
