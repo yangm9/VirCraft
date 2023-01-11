30582959bb89d697974a98cb03fcf2c340622f1d
yangm9 <yangm9@icloud.com>
2023-01-11 16:37:22 +0800

    v0.0.5

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@ wget -c https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 sh Miniconda3-latest-Linux-x86_64.sh -b
 conda init
 source ~/.bashrc
-pip install -r requirements.txt
 conda create -y -n VirCraft megahit -c bioconda
 conda activate VirCraft
 conda install -y -c bioconda bwa=0.7.17 blast=2.13.0 trimmomatic=0.39 \
