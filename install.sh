88a88db46cfde6aa6bfadce3a4388f9d4a6fcf7c
yangm9 <yangm9@icloud.com>
2023-03-15 15:35:04 +0800

    v0.0.6

--- install.sh
+++ install.sh
@@ -9,18 +9,16 @@ else
     source ~/.bashrc
     echo "The installation of conda is complete！"
 fi
-conda create -y -n emapper eggnog-mapper= -c bioconda
-conda activate emapper
-conda install -y -c bioconda diamond=2.0.14
-conda create -y -n VirCraft megahit -c bioconda
+conda create -y -c bioconda -n VirCraft cd-hit=4.8.1
 conda activate VirCraft
-conda install -y -c bioconda diamond=2.0.14 #dimond must be thisversion.
-conda install -y -c bioconda spades=3.15.4
-conda install -y -c bioconda bwa=0.7.17 blast=2.13.0 trimmomatic=0.39 \
-prodigal=2.6.3 fastp=0.23.2 sortmerna=4.3.4 bowtie2=2.4.4 virsorter=2.2.3 \
-checkv=0.8.1 dram=1.3.5 cd-hit=4.8.1 vibrant=1.2.1 iqtree=2.2.0.3 \
-coverm=0.6.1 crass=1.0.1 minced=0.4.2 trnascan-se=2.0.11 salmon=0.14.2 \
-hmmer=3.3.2 muscle=5.1 drep=3.3.1 aragorn=1.2.41 kofamscan=1.3.0 fastuniq=1.1
+conda install -y -c bioconda trimmomatic=0.39 fastp=0.23.2 sortmerna=4.3.4 fastuniq=1.1
+conda install -y -c bioconda megahit=1.2.9 spades=3.15.4
+conda install -y -c bioconda diamond=2.0.14 blast=2.13.0 #dimond must be thisversion.
+conda install -y -c bioconda bwa=0.7.17 bowtie2=2.4.4
+conda install -y -c conda-forge -c bioconda checkv=1.0.1
+conda install -y -c bioconda virsorter=2.2.3 dram=1.3.5 vibrant=1.2.1
+conda install -y -c bioconda prodigal=2.6.3 
+conda install -y -c bioconda eggnog-mapper=2.1.10 kofamscan=1.3.0 iqtree=2.2.0.conda install -y -c bioconda coverm=0.6.1 crass=1.0.1 minced=0.4.2 trnascan-se=2.0.11 salmon=0.14.2 hmmer=3.3.2 muscle=5.1 drep=3.3.1 aragorn=1.2.41
 conda install -c conda-forge r-base=4.2.1 r-ggplot2=3.4.0 r-vctrs=0.5.1
 conda install -c conda-forge scikit-learn-intelex
 conda install -c bioconda megan
