48db4b3eb416d66755231f4d0cf02651d5fc608b
yangm9 <yangm9@icloud.com>
2023-03-17 22:34:06 +0800

    v0.0.6

--- crafts/general/utils.py
+++ crafts/general/utils.py
@@ -41,16 +41,16 @@ def isInstalled(name:str):
     from shutil import which
     return which(name)
 
-def execute(cmd:list, silent=False):
+def run(cmd:list, silent=False):
     '''
     :ref: virmatcher, https://github.com/bolduc/kb_virmatcher
     :param command: Command suitable for running in subprocess, must use a ['ls', '-l'] format
     :param silent: Run silently
     :return: Response from command
     '''
-    print(f'Running command:\n{cmd_txt}') 
     cmd_txt=' '.join(cmd).replace('\n ','\n')
     cmd_txt=cmd_txt.replace(' \n ','\n')
+    print(f'Running command:\n{cmd_txt}') 
     if silent:
         results=subprocess.run(cmd,shell=False,encoding='utf-8',check=True,stdout=subprocess.DEVNULL,stderr=subprocess.PIPE)
     else:
@@ -59,6 +59,13 @@ def execute(cmd:list, silent=False):
     if results.returncode != 0:
         print(f'Error running command: {cmd_txt}. The error message was:\n{results.stderr}')
         exit(1)
+    return results
+
+def execute(cmd):
+    cmd_txt=' '.join(cmd).replace('\n ','\n')
+    cmd_txt=cmd_txt.replace(' \n ','\n')
+    print(f'Running command:\n{cmd_txt}')
+    results=os.system(cmd_txt)
     return results
 
 def show_cmd(cmd):
