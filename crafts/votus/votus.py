493a61a9ab3756294ebbc7bda2c5d74baa8dc67a
yangm9 <yangm9@icloud.com>
2023-04-28 20:11:04 +0800

    v0.0.8

--- crafts/votus/votus.py
+++ crafts/votus/votus.py
@@ -16,14 +16,26 @@ class VirRef(VirScan):
         cmd=['cd-hit-est','-i',self.fasta,'-o',votus,'-T',self.threads,
             '-c 0.95 -aS 0.85 -n 10 -d 0 -M 160000\n']
         return cmd,votus
-    def RmDup(self,cutoff=5000,unrun=False):
+    def votuQC(self,votus,cutoff=1500):
+        cmd=[utils.selectENV('viral-id-sop')]
+        tmp_cmd,__=self.checkv(votus)
+        cmd.extend(tmp_cmd)
+        wkdir=f'{self.outdir}/stat'
+        ckdir=f'{self.outdir}/checkv'
+        checkv_qual=f'{ckdir}/checkv/quality_summary.tsv'
+        cmd.extend(['pie_plot.R',checkv_qual,'checkv_quality',wkdir])
+        cmd.extend(['pie_plot.R',checkv_qual,'provirus',wkdir])
+        cmd.extend(['pie_plot.R',checkv_qual,'miuvig_quality',wkdir])
+        tmp_cmd=self.statFA(cutoff)
+        cmd.extend(self.statFA(cutoff))
+        cmd.extend(tmp_cmd)
+        return cmd
+        
+    def RmDup(self,cutoff=1500,unrun=False):
         cmd=[self.envs]
         tmp_cmd,votus=self.cluster()
         cmd.extend(tmp_cmd)
-        tmp_cmd,tmp_fa=self.checkv(votus)
-        cmd.extend(tmp_cmd)
-        tmp_cmd=self.statFA(cutoff)
-        cmd.extend(tmp_cmd)
+        cmd.extend(self.votuQC(votus,cutoff))
         shell=f'{self.outdir}/{self.name}_votu.sh'
         utils.printSH(shell,cmd)
         results=''
