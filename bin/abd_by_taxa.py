8ba5e01f78367e7cef743d8cb54a974b3f62b664
yangm9 <yangm9@icloud.com>
2023-05-02 20:54:31 +0800

    v0.0.8

--- bin/abd_by_taxa.py
+++ bin/abd_by_taxa.py
@@ -5,17 +5,26 @@
 import sys
 import pandas as pd
 
-def sumAbundByTaxa(taxa_tpm: str, outdir: str):
-    '''
-    Sum the abundance by Taxa for each sample.
-    input format:
-    Contig\tSample1 Abundance\t...\tSampleN Abundance\n
-    '''
-    df=pd.read_csv(taxa_tpm,sep='\t',header=0)
+#input format:
+#Contig\tSample1 Abundance\t...\tSampleN Abundance\n
+#Contig format: "contig_id_12345:Caudovirales;Myoviridae"
+#taxa:
+#taxonomic level, including Order, Family and All.
+taxa_loc_dict={'Order':0,'Family':1}
+def idxAbd(taxa_abd:str,taxa='taxa',outdir:str):
+    df=pd.read_csv(taxa_abd,sep='\t',header=0)
     samp_num=df.columns.size
     df=df.iloc[:,0:samp_num]
     df['Contig']=df['Contig'].apply(lambda x:x.split(':')[1])
-    df['Contig'].replace('nan;nan','Unassigned',inplace=True)
+    if taxa=='taxa':
+        df['Contig'].replace('nan;nan','Unassigned',inplace=True)
+    else:
+        df['Contig']=df['Contig'].apply(lambda x:x.split(';')[taxa_loc_dict[taxa]])
+        df['Contig'].replace('nan','Unassigned',inplace=True)
+    return df
+
+#Sum the abundance by Taxa for each sample.
+def sumAbds(df:str,taxa='taxa',outdir:str)
     TaxaList=df['Contig'].unique().tolist()
     df=df.set_index('Contig')
     sumDF=pd.DataFrame()
@@ -28,12 +37,21 @@ def sumAbundByTaxa(taxa_tpm: str, outdir: str):
     sumDF.reset_index(inplace=True)
     sumDF.rename(columns={'index':'Contig'},inplace=True)
     sumDF.fillna(0,inplace=True)
-    sumed_tpm=f'{outdir}/all_taxa_sum_abd.xls'
-    sumDF.to_csv(sumed_tpm,sep='\t',index=False)
+    sumed_abd=f'{outdir}/all_{taxa}_sum_abd.xls'
+    sumDF.to_csv(sumed_abd,sep='\t',index=False)
+    return 0
+
+def sumAbdByTaxa(taxa_abd:str,outdir:str):
+    df=idxAbd(taxa_abd,'taxa',outdir)
+    sumAbds(df,'taxa',outdir)
+    df=idxAbd(taxa_abd,'Order',outdir)
+    sumAbds(df,'Order',outdir)
+    df=idxAbd(taxa_abd,'Family',outdir)
+    sumAbds(df,'Family',outdir)
     return 0
 
 if __name__=='__main__':
     if len(sys.argv)==3:
-        sumAbundByTaxa(sys.argv[1],sys.argv[2])
+        sumAbdByTaxa(sys.argv[1],sys.argv[2])
     else:
-        print(f'Usage: python {sys.argv[0]} <merged_anno_tpm.xls> <outdir>')
+        print(f'Usage: python {sys.argv[0]} <merged_anno_abd.xls> <outdir>')
