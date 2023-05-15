609cf30afa1960189692865bb5af44ce1100b6b3
yangm9 <yangm9@icloud.com>
2023-05-15 10:19:08 +0800

    v0.0.8

--- bin/extract_subseq_by_bed.py
+++ bin/extract_subseq_by_bed.py
@@ -1,41 +1,29 @@
+import sys
 from Bio import SeqIO
 
-def extract_sequence(sequence, start, end):
-    """
-    从给定的序列中提取指定位置的子序列
-    :param sequence: str, 输入的序列
-    :param start: int, 起始位置
-    :param end: int, 终止位置
-    :return: str, 提取的子序列
-    """
+def extrSeq(sequence, start, end):
     return sequence[start-1:end]
 
-# 读取fasta文件
-fasta_file = "input.fasta"
-fasta_sequences = SeqIO.parse(open(fasta_file),'fasta')
+def getSubfaByBed(fasta_file,bed_file,subfa_file):
+    fasta_sequences = SeqIO.parse(open(fasta_file),'fasta')
+    #output_file=os.path.splitext(fasta_file)[0]
+    SUBFA=open(output_file,"w")
+    with open(bed_file, "r") as f:
+        for line in f:
+            fields = line.strip().split("\t")
+            chrom = fields[0]
+            start = int(fields[1])
+            end = int(fields[2])
+            name = fields[3]
+            for fasta in fasta_sequences:
+                if fasta.id == chrom:
+                    sequence = str(fasta.seq)
+                    subsequence = extrSeq(sequence, start, end)
+                    SUBFA.write(">" + name + "\n")
+                    SUBFA.write(subsequence + "\n")
+                    break
+    fasta_sequences.close()
+    return 0
 
-# 读取bed文件
-bed_file = "input.bed"
-with open(bed_file, "r") as f:
-    for line in f:
-        fields = line.strip().split("\t")
-        chrom = fields[0]
-        start = int(fields[1])
-        end = int(fields[2])
-        name = fields[3]
-        
-        # 提取子序列
-        for fasta in fasta_sequences:
-            if fasta.id == chrom:
-                sequence = str(fasta.seq)
-                subsequence = extract_sequence(sequence, start, end)
-                
-                # 将子序列写入输出文件
-                output_file = name + ".fasta"
-                with open(output_file, "w") as out:
-                    out.write(">" + name + "\n")
-                    out.write(subsequence + "\n")
-                break
-
-# 关闭fasta文件
-fasta_sequences.close()
+if __name__=='__main__':
+    getSubfaByBed(sys.argv[1],sys.argv[2],sys.argv[3])
