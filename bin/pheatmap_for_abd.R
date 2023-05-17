5a39f8fdd9c3d6316398142bd2a121fd4b9d81ee
yangm9 <yangm9@icloud.com>
2023-05-17 20:16:59 +0800

    v0.0.8

--- bin/pheatmap_for_abd.R
+++ bin/pheatmap_for_abd.R
@@ -20,8 +20,8 @@ grp_color <- c("#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd",
                "#aec7e8", "#ffbb78", "#98df8a", "#ff9896", "#c5b0d5",
                "#c49c94", "#f7b6d2", "#c7c7c7", "#dbdb8d", "#9edae5")
 
-source_color <- c("#8dd3c7", "#fdb462", "#b3de69", "#82c1a8", "#d9d9d9",
-                  "#fffac8", "#c34b48", "#a163ad", "#ff1493", "#00c1ff",
+source_color <- c("#8dd3c7", "#fdb462", "#b3de69", "#00688b", "#d9d9d9",
+                  "#c34b48", "#fffac8", "#a163ad", "#ff1493", "#00c1ff",
                   "#a87c00", "#619cff", "#f8766d", "#008000", "#964500",
                   "#f6b3c3", "#5b8cb2", "#d2d28e", "#b297c9", "#ccebc5",
                   "#cc3a21", "#00ba38", "#a15d98", "#004a7f", "#ffc0cb",
@@ -31,8 +31,7 @@ source_color <- c("#8dd3c7", "#fdb462", "#b3de69", "#82c1a8", "#d9d9d9",
                   "#008080", "#b41e00", "#6c7c32", "#cc8899", "#8f00ff",
                   "#ffa500", "#3b3b3b", "#ff0000", "#006400", "#00a3a3",
                   "#cd950c", "#00688b", "#8b795e", "#458b74", "#9acdf2",
-                  "#ee9a00", "#00ff00", "#bebebe", "#9ac73f", "#beaed4",
-                  "#cd950c", "#00688B")
+                  "#ee9a00", "#00ff00", "#bebebe", "#9ac73f", "#beaed4")
 
 df<-read.table(
     argv[1],header=T,sep="\t",
