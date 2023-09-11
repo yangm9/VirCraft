7c4f5d3b0ee5ab5f92394ae7fa03e1052b88246e
yangm9 <yangm9@icloud.com>
2023-09-11 20:47:32 +0800

    v0.0.10

---
+++ crafts/setup/dburl.py
@@ -0,0 +1,12 @@
+#NCBI Viral RefSeq and Taxonomy
+NCBI_VIR_PROT_URL='https://ftp.ncbi.nlm.nih.gov/refseq/release/viral/viral.1.protein.faa.gz'
+NCBI_RELEASE_NUMBER_URL='https://ftp.ncbi.nlm.nih.gov/refseq/release/RELEASE_NUMBER'
+NCBI_TAXDUMP_URL='ftp://ftp.ncbi.nih.gov/pub/taxonomy/taxdump.tar.gz'
+
+#KofamScan Database
+KO_LIST_URL='ftp://ftp.genome.jp/pub/db/kofam/ko_list.gz'
+KO_PROFILES_URL='ftp://ftp.genome.jp/pub/db/kofam/profiles.tar.gz'
+
+#eggNOG Database
+EGGNOG_DB_URL='http://eggnog6.embl.de/download/emapperdb-5.0.2/eggnog.db.gz'
+EGGNOG_DMND_URL='http://eggnog6.embl.de/download/emapperdb-5.0.2/eggnog_proteins.dmnd.gz'
--- crafts/setup/deployDB.py
+++ crafts/setup/deployDB.py
@@ -1,6 +1,7 @@
 import os
 from datetime import datetime
 from ..general import utils
+from . import dburl
 
 class DB:
     '''
@@ -9,39 +10,83 @@ class DB:
     def __init__(self,outdir='',threads=8):
         self.outdir=outdir
         self.threads=str(threads)
-    def dl_refseq_viral_prot(self):
-        wkdir=f'{self.outdir}/tax_db_dir'
-        utils.mkdir(wkdir)
-        vir_prot_url='https://ftp.ncbi.nlm.nih.gov/refseq/release/viral/viral.1.protein.faa.gz'
-        vir_prot=f'{wkdir}/viral.1.protein.faa.gz'
-        cmd=['wget -c',vir_prot_url,'-O',vir_prot,'--no-check-certificate\n',
-            'gzip -d',vir_prot,'\n']
-        return cmd
-    def dl_viridsop_db(self):
-        wkdir=f'{self.outdir}/vs2_db'
+    def dl_virsorter2_db(self):
+        wkdir=f'{self.outdir}/VC-VirSorter2DB'
         utils.mkdir(wkdir)
         cmd=['conda run -n viral-id-sop virsorter setup',
-             '-d',wkdir,'-j',threads,'\n']
+             '-d',wkdir,'-j',self.threads,'\n']
+        return cmd
+    def dl_vibrant_db(self):
+        wkdir=f'{self.outdir}/VC-VIBRANTDB'
+        utils.mkdir(wkdir)
+        cmd=['conda run -n vibrant download-db.sh',wkdir,'\n']
         return cmd
     def dl_checkv_db(self):
-        wkdir=f'{self.outdir}'
+        wkdir=f'{self.outdir}/VC-CheckVDB'
         utils.mkdir(wkdir)
         cmd=['conda run -n viral-id-sop checkv download_database',wkdir,'\n']
         return cmd
-    
+    def dl_refseq_viral_prot(self):
+        wkdir=f'{self.outdir}/VC-ViralRefSeqDB'
+        utils.mkdir(wkdir)
+        taxdump_tgz=f'{wkdir}/taxdump.tar.gz'
+        vir_prot_prefix=f'{wkdir}/viral.1.protein'
+        vir_prot=vir_prot_prefix+'.faa'
+        vir_prot_gz=vir_prot+'.gz'
+        vir_pid_sp=f'{wkdir}/NCBI_viral_pid_sp.txt'
+        vir_name_taxaid=vir_pid_sp.replace('_pid_sp','_name_taxid')
+        vir_pid_taxa=vir_pid_sp.replace('_pid_sp','_taxnomomy')
+        vir_sp_taxa=vir_pid_sp.replace('_pid_sp','_sp_taxa')
+        vir_pid_sp_h=vir_pid_sp.replace('.txt','.h.txt')
+        vir_full_taxa=vir_pid_sp.replace('_pid_sp','_full_taxnomomy')
+        cmd=['wget','-c',dburl.NCBI_VIR_PROT_URL,'-O',vir_prot_gz,
+            '--no-check-certificate\n','gzip -d',vir_prot_gz,'\n',
+            'wget','-c',dburl.NCBI_RELEASE_NUMBER_URL,'-O',vir_prot,
+            '--no-check-certificate\n','makeblastdb -in',vir_prot_gunzip,
+            '-parse_seqids -hash_index','-out',vir_prot_prefix,'-dbtype prot\n',
+            'wget','-c',dburl.NCBI_TAXDUMP_URL,'-O',taxdump_tgz,'--no-check-certificate\n',
+            'mkdir ~/.taxonkit && tar xzf',taxdump_tgz,'-c ~/.taxonkit\n',
+            'extract_name.py',vir_prot,'>',vir_pid_sp,'\n',
+            'cut -f 2',vir_pid_sp,"|uniq|taxonkit name2taxid|sed '1ispecies\ttaxid'>",vir_name_taxaid,'\n',
+            'cut -f 2',vir_pid_sp,"|uniq|taxonkit name2taxid|cut -f 2|taxonkit lineage|taxonkit reformat -r 'Unassigned'|cut -f 1,3|sed '1itaxid\ttaxonomy'>",vir_pid_taxa,'\n',
+            "csvtk join -t -f 'taxid;taxid'",vir_name_taxaid,vir_pid_taxa,
+            '|uniq>',vir_sp_taxa,'\n',
+            'csvtk add-header -t -n NCBI_ID,species',vir_pid_sp,
+            '>',vir_pid_sp_h,'\n',
+            "csvtk join -t -f 'species;species'",vir_sp_taxa,vir_pid_sp_h,
+            '>',vir_full_taxa,'\n',
+            'rm -f',vir_pid_sp,vir_name_taxaid,vir_pid_taxa,vir_sp_taxa,vir_pid_sp_h,'\n']
+        return cmd
+    def dl_kofamscan_db(self):
+        wkdir=f'{self.outdir}/VC-KofamScanDB'
+        utils.mkdir(wkdir)
+        ko_list_gz=f'{wkdir}/ko_list.gz'
+        profiles_tgz=f'{wkdir}/profiles.tar.gz'
+        cmd=['wget -c',dburl.KO_LIST_URL,'-O',ko_list_gz'\n',
+            'gunzip',ko_list_gz,'\n',
+            'wget -c',dburl.KO_PROFILES_URL,'-O',profiles_gz'\n',
+            'tar xzf',profiles_tgz,'-C',wkdir,'\n']
+        return cmd
     def dl_dramv_db(self):
-        wkdir=f'{self.outdir}/dramv_db'
+        wkdir=f'{self.outdir}/VC-DRAMvDB'
         utils.mkdir(wkdir)
         cmd=['conda run -n viral-id-sop DRAM-setup.py prepare_databases',
              '--skip_uniref','--output_dir',wkdir,'\n']
         return cmd
-    def dl_vb_db(self):
-        wkdir=f'{self.outdir}/vb_db'
+    def dl_eggnog_db(self):
+        wkdir=f'{self.outdir}/VC-eggNOGDB'
         utils.mkdir(wkdir)
-        cmd=['conda run -n vibrant download-db.sh',wkdir,'\n']
+        eggnog_db_gz=f'{wkdir}/eggnog.db.gz'
+        eggnog_dmnd_gz=f'{wkdir}/eggnog_proteins.dmnd.gz'
+        cmd=['wget -c',dburl.EGGNOG_DB_URL,'-O',eggnog_db_gz,
+        '&& gunzip',eggnog_db_gz,'\n',
+        'wget -c',dburl.EGGNOG_DMND_URL,'-O',eggnog_dmnd_gz,
+        '&& gunzip',eggnog_dmnd_gz,'\n']
         return cmd
-#    def 
-#    def dl_all(self):
+    def deployDB(self):
+        cmd=self.dl_virsorter2_db()
+        cmd.extend(self.dl_vibrant_db())
+        cmd.extend(self.dl_checkv_db())
+        cmd.extend(self.dl_refseq_viral_prot())
+        cmd.extend(self.dl_dramv_db())
         
-
-
