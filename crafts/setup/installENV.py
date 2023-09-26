5a57245ac3e5eccf392fd22d6aa57b84b8f2622e
yangm9 <yangm9@icloud.com>
2023-09-26 18:14:41 +0800

    v0.0.11

--- crafts/setup/installENV.py
+++ crafts/setup/installENV.py
@@ -1,4 +1,5 @@
 import sys
+import subprocess
 from datetime import datetime
 from ..general import utils
 from . import URL
@@ -14,6 +15,22 @@ class ENV:
         self.outdir=outdir
         self.threads=str(threads)
         utils.mkdir(self.outdir)
+    def is_conda_env(env_name):
+        try:
+            # 使用conda命令列出已安装的环境
+            result=subprocess.run(['conda', 'env', 'list'], capture_output=True,text=True,check=True)
+            output_lines=result.stdout.splitlines()
+            # 检查每行输出是否包含指定的环境名称
+            env_exist=any(env_name in line for line in output_lines)
+            if env_exist:
+                return 1
+            else:
+                return 0
+        except subprocess.CalledProcessError as e:
+            # 如果无法执行conda命令，则返回-1表示出错
+            print(f'ERROR: {e}')
+            exit(1)
+            return -1
     def setup_env(self,name,in_wall=False):
         net=''
         if in_wall: net='_cn'
@@ -23,8 +40,10 @@ class ENV:
     def Install(self,in_wall=False,unrun=False):
         for env in self.ENVLIST:
             cmd=self.setup_env(env,in_wall)
-            shell=f'{self.outdir}/{env}_install.sh'
-            utils.printSH(shell,cmd)
-            results=''
-            if not unrun: results+=utils.execute(cmd)
+            is_env=self.is_conda_env(env)
+            if not is_env:
+                shell=f'{self.outdir}/{env}_install.sh'
+                utils.printSH(shell,cmd)
+                results=''
+                if not unrun: results+=utils.execute(cmd)
         return results
