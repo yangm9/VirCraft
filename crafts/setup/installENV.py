9970e45153889722537f6883e602f8211b14449f
yangm9 <yangm9@icloud.com>
2023-09-27 12:46:10 +0800

    v0.0.11

--- crafts/setup/installENV.py
+++ crafts/setup/installENV.py
@@ -9,16 +9,20 @@ class ENV:
     Install the environments of VirCraft
     '''
     CONDAENVDIR=f'{sys.path[0]}/crafts/setup/conda_env_yaml'
-    ENVLIST=['reads_qc','assembly','virsorter2','vibrant','deepvirfinder',
-            'dramv','checkv','vcontact','gtdbtk','quantify','general']
+    ENVDICT={'reads_qc':'VC-ReadsQC','assembly':'VC-Assembly',
+             'virsorter2':'VC-VirSorter2','vibrant':'VC-VIBRANT',
+             'deepvirfinder':'VC-DeepVirFinder','checkv':'VC-CheckV',
+             'dramv':'VC-DRAMv','vcontact':'VC-vContact2',
+             'gtdbtk':'VC-GTDBTk','quantify':'VC-Quantify',
+             'general':'VC-General'}
     def __init__(self,outdir='',threads=8):
         self.outdir=outdir
         self.threads=str(threads)
         utils.mkdir(self.outdir)
-    def is_conda_env(env_name):
+    def is_conda_env(self,env_name):
         try:
             # 使用conda命令列出已安装的环境
-            result=subprocess.run(['conda', 'env', 'list'], capture_output=True,text=True,check=True)
+            result=subprocess.run(['conda','env','list'],capture_output=True,text=True,check=True)
             output_lines=result.stdout.splitlines()
             # 检查每行输出是否包含指定的环境名称
             env_exist=any(env_name in line for line in output_lines)
@@ -38,12 +42,14 @@ class ENV:
         cmd=['mamba env create','-f',env_yaml,'\n']
         return cmd
     def Install(self,in_wall=False,unrun=False):
-        for env in self.ENVLIST:
+        for env in self.ENVDICT.keys():
+            is_env=self.is_conda_env(self.ENVDICT[env])
+            if is_env:
+                print(f'{env} is already installed, so skipping!')
+                continue
             cmd=self.setup_env(env,in_wall)
-            is_env=self.is_conda_env(env)
-            if not is_env:
-                shell=f'{self.outdir}/{env}_install.sh'
-                utils.printSH(shell,cmd)
-                results=''
-                if not unrun: results+=utils.execute(cmd)
+            shell=f'{self.outdir}/{env}_install.sh'
+            utils.printSH(shell,cmd)
+            results=''
+            if not unrun: results+=utils.execute(cmd)
         return results
