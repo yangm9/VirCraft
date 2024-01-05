0fb5c5b3e621d681e0be76141c3b26d2fc6cfb20
yangm9 <yangm9@icloud.com>
2024-01-05 16:24:45 +0800

    v0.0.12

--- bin/sum_abd_by_seq.py
+++ bin/sum_abd_by_seq.py
@@ -1,1 +1,12 @@
-sumAbundance.py
+#!/usr/bin/env python3
+import re
+import sys
+import pandas as pd
+
+df=pd.read_csv(sys.argv[1],sep='\t')
+samp_num=df.columns.size
+df['Total_Abundance']=df.iloc[:,1:samp_num].sum(axis=1)
+df=df[['Contig','Total_Abundance']]
+pattern=re.compile('\:\w+;\w+$')
+df['Contig']=df['Contig'].apply(lambda x:re.sub(pattern,'',x))
+df.to_csv(sys.argv[2],sep='\t',index=False)
--- bin/taxa_annot_abd.py
+++ bin/taxa_annot_abd.py
@@ -1,1 +1,14 @@
-tpmAddSource.py
+#!/usr/bin/env python3
+import re
+import sys
+import pandas as pd
+
+df=pd.read_csv(sys.argv[1],sep='\t')
+df['Order']=df['Order'].astype('str')
+df['Family']=df['Family'].astype('str')
+#df['Source']=df['Contig'].apply(lambda x:x.split('_')[0] if re.search(r'_',x) else x)
+#df['Length']=df['Contig'].apply(lambda x:x.split('_')[4])
+df['Contig']=df['Contig'].apply(lambda x:re.sub(r'_length_\d+_cov_\d+\.\d*_\d*','',x))
+df['Contig']=df['Contig']+':'+df['Order']+';'+df['Family']
+df.drop(columns=['Order','Family'],inplace=True)
+df.to_csv(sys.argv[2],sep='\t',index=False)
