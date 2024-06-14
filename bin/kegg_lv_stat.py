b4963aba4900edfbeb76f9a7d91b933ceab41e52
yangm9 <yangm9@icloud.com>
2024-06-14 15:29:57 +0800

    v0.0.13

--- bin/kegg_lv_stat.py
+++ bin/kegg_lv_stat.py
@@ -4,19 +4,19 @@ import sys
 def kegg_lv_stat(kegg_anno_f:str,lv_name:str):
     with open(kegg_anno_f) as f:
         gene_dict={}
-        header=f.readline()
-        col_idx=header.index(lv_name)
+        headers=f.readline().split('\t')
+        col_idx=headers.index(lv_name)
         for line in f:
             items=line.strip().split('\t')
             gene_id=items[0]
-            level2=items[col_idx]
-            gene_dict.setdefault(level2,[])
-            if not gene_id in gene_dict[level2]:
-                gene_dict[level2].append(gene_id)
-    for level2,gene_id_list in gene_dict.items():
+            level=items[col_idx]
+            gene_dict.setdefault(level,[])
+            if not gene_id in gene_dict[level]:
+                gene_dict[level].append(gene_id)
+    for level,gene_id_list in gene_dict.items():
         set_length=len(set(gene_id_list))
-        print(level2+'\t'+str(set_length))
-return 0
+        print(level+'\t'+str(set_length))
+    return 0
 
 if __name__=='__main__':
     kegg_lv_stat(sys.argv[1],sys.argv[2])
