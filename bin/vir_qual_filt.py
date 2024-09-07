06af116d75d2a40185a591747b7c294417f7fd44
yangm9 <yangm9@icloud.com>
2024-09-07 23:59:49 +0800

    v0.0.13

--- bin/vir_qual_filt.py
+++ bin/vir_qual_filt.py
@@ -3,9 +3,16 @@ import sys
 import pandas as pd
 
 def filt_checkv(checkv_d:str):
+    '''
+    input: full path of checkv directory
+    methods: the vs2-vb-dvf identified viral contigs that are ≥ 5 kb or those that are circular and ≥ 1.5 kb were considered as high-confidence viral contigs.
+    output: virus_quality_summary.filt.tsv and provir_quality_summary.filt.tsv
+    return: viral ID list and proviral ID list
+    '''
+    #Filter conditions
     virus_qual_filt_condition='provirus=="No" and (contig_length>=5000 or (contig_length>1500 and checkv_quality=="Complete"))'
     provir_qual_filt_condition='provirus=="Yes" and (proviral_length>=5000 or (proviral_length>1500 and checkv_quality=="Complete"))'
-
+    #
     qual_summ_tsv=f'{sys.argv[1]}/quality_summary.tsv'
     virus_qual_summ_tsv=qual_summ_tsv.replace('quality_summary.tsv','virus_quality_summary.filt.tsv')
     provir_qual_summ_tsv=qual_summ_tsv.replace('quality_summary.tsv','provir_quality_summary.filt.tsv')
